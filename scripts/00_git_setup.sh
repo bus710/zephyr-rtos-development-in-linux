9f472348e5dc61de758c6c271913af555b2e3d0b
Seongjun Kim <bus710@gmail.com>
2025-04-20 12:16:37 -0700

    progress

--- scripts/00_git_setup.sh
+++
@@ -1,4 +0,0 @@
-git config remote.origin.url https://bus710@github.com/bus710/zephyr-rtos-development-in-linux
-git config --global user.email "bus710@gmail.com"
-git config --global user.name "bus710"
-git config --global push.default simple
